765e5e2e2ad7977d1d460f9b4482d9a05cb478a2
ilya6300 <ilya63001990@mail.ru>
2025-10-09 10:20:46 +0300

    ипр

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
